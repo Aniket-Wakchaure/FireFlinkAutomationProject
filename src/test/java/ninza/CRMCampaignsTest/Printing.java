c420709fca3daf1a68f783c7cddca93217b831a6
Wakchaure Aniket Mac <Wakchaure Aniket Mac@FFE-Wakchaure-Aniket-Machhindra>
2025-08-23 12:01:03 +0530

    Print Commited

---
+++ src/test/java/ninza/CRMCampaignsTest/Printing.java
@@ -0,0 +1,13 @@
+package ninza.CRMCampaignsTest;
+
+import org.testng.annotations.Test;
+
+public class Printing {
+	@Test(groups = "SmokeSuit")
+	public void print() {
+		
+		System.out.println("============= Printinng Done=================");
+	
+	}
+
+}
